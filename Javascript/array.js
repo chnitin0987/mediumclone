20205ad4beeed09716cc0fb9f024f0e8215e35bf
chnitin0987 <connect.nitin1409@gmail.com>
2026-06-15 17:48:16 +0530

    Initial commit

---
+++ Javascript/array.js
@@ -0,0 +1,17 @@
+// let arr = [15 , 20 , 52 , 85 , 95 , 32 , 35]
+// arr.splice(2 , 0 ,35)
+// for (let i = 0 ; i <arr.length ; i++) {
+//     console.log(arr[i])
+// }
+
+
+// let user = {
+//     name: "nitin kumar",
+//     age: 25 ,
+//     isStudent: true ,
+//     "": "Empty String",
+//     "  ": 17636524,
+// } ;
+// console.log(user); 
+// console.log(user[""])
+// console.log(user["  "])
---
+++ Javascript/loops.js
@@ -0,0 +1,31 @@
+// for (let a = 0 ; a < 10 ; a++) {
+//     console.log(a)
+// }
+
+// let count = 5
+// while (count > 0) {
+//     console.log("countdown - ",count)
+//     count--
+// }
+
+
+// for (let i = 5; i >= 1; i--) {
+//     let m = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         m += "*";
+//     }
+
+//     console.log(m);
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let x = "";
+
+//     for (let j = 1; j <= i; j++) {
+//         x += "*";
+//     }
+
+//     console.log(x);
+// }
